e8027458c5b1a3827ca1269566e96bba361f1f5f
lisekxd1234 <lisekxd1234@gmail.com>
2024-04-14 12:42:51 +0200

    Dodanie zadania4 do branch zad4

--- Company.java
+++ Company.java
@@ -4,26 +4,24 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Karol Bielecki", 3500, 0);
         employees[1] = new Worker("Julia Wojciechowska", 4200, "Accountant");
         employees[2] = new Employee("Robert Łukasik", 3900);
         employees[3] = new Employee("Ewa Baran", 4700);
         employees[4] = new Worker("Tomasz Leśniak", 3000, "HR Specialist");
+        employees[5] = new Manager("Alicja Nowak", 3400, 0);
+        employees[6] = new Worker("Krzysztof Krawczyk", 3600, "Developer");
 
-        int nonManagerCount = 0;
         for (Employee emp : employees) {
-            if (!(emp instanceof Manager)) {
-                nonManagerCount++;
-            }
+            emp.setSalary(emp.getSalary() + 500);
         }
 
-        if (employees[0] instanceof Manager) {
-            ((Manager)employees[0]).setNumberOfSubordinates(nonManagerCount);
-        }
-
-        employees[0].setSalary(7500);
+        ((Manager)employees[0]).setNumberOfSubordinates(6);
+        ((Manager)employees[0]).setSalary(7500);
+        ((Manager)employees[5]).setNumberOfSubordinates(6);
+        ((Manager)employees[5]).setSalary(7500);
 
         System.out.println("Lista wszystkich pracowników:");
         for (Employee emp : employees) {
